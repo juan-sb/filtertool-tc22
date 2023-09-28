2f9e00a37eb1ca9dc7f5e746876b0325d7e4370f
juan-sb <sbruzzi.juan@outlook.com>
2023-09-28 01:21:45 -0300

    Trash fix for legendre

--- src/package/Filter.py
+++ src/package/Filter.py
@@ -255,17 +255,19 @@ class AnalogFilter():
         
         elif self.approx_type == LEGENDRE:
             self.N = self.N_min
+            # eps == xi
             eps = np.sqrt(((10 ** (-0.1 * self.gp_dB)) - 1))
-
+            
             while True:
                 L_eps = get_Leps(self.N, eps)
                 z = []
                 p = select_roots(L_eps)
                 p0 = np.prod(p) * (1 if self.N % 2 == 0 else -1) #en N tengo N polos y yo quiero obtener el producto de los polos negados para normalizar
                 tf2 = TFunction(z, p, p0)
-                tf2_wmin = abs(tf2.at(1j))
+                p0 /= tf2.at(1j)
                 tf2_wmax = abs(tf2.at(self.wan*1j))
-                if(self.N == self.N_max or (tf2_wmin >= self.gp and tf2_wmax <= self.ga)):
+                # print(self.N, tf2_wmin >= self.gp, tf2_wmax <= self.ga, tf2_wmin, tf2_wmax)
+                if(self.N == self.N_max or tf2_wmax <= self.ga):
                     self.tf_norm = TFunction(z, p, p0)
                     break
                 self.N += 1
